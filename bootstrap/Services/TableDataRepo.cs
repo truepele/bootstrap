a855eea82a992fed3f574e200f7862c3c61a6748
truepele <truepele@gmail.com>
2016-07-14 23:38:10 -0700

    Server-side search implemented

--- bootstrap/Services/TableDataRepo.cs
+++ bootstrap/Services/TableDataRepo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace bootstrap.Services
@@ -18,12 +19,36 @@ namespace bootstrap.Services
 
         public List<string[]> GetData(int start, int length, string searchText, bool isSearchRegex, out int totalrecords)
         {
-            totalrecords = TotalRecords;
-
-            var dataList = ConstructData(start, length, totalrecords);
-
+            List<string[]> dataList;
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                dataList = ConstructData(0, TotalRecords, TotalRecords);
+                dataList = FilterDataList(dataList, searchText, isSearchRegex);
+                totalrecords = dataList.Count;
+                dataList = dataList.Skip(start).Take(length).ToList();
+            }
+            else
+            {
+                dataList = ConstructData(start, length, TotalRecords);
+                totalrecords = TotalRecords;
+            }
+            
             return dataList;
         }
+
+        private static List<string[]> FilterDataList(List<string[]> dataList, string searchText, bool isSearchRegex = false)
+        {
+            dataList = (from iArr in dataList
+                where
+                    iArr.FirstOrDefault(
+                        str =>
+                            isSearchRegex
+                                ? Regex.IsMatch(str, searchText, RegexOptions.IgnoreCase)
+                                : str.IndexOf(searchText, StringComparison.InvariantCultureIgnoreCase) >= 0) != null
+                select iArr).ToList();
+            return dataList;
+        }
+
 
         private static List<string[]> ConstructData(int start, int length, int totalrecords)
         {
